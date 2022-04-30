735adc9d1a191c7e082e619bfdecb15357c91264
c1rfa <svoron@gmail.com>
2022-04-30 16:19:24 +0300

    added second task

---
+++ email_gen/email_gen.py
@@ -0,0 +1,40 @@
+from os import sep
+import random
+import re
+
+import pandas as pd
+
+
+def generate_email_address ():
+
+    #website name
+    websites = pd.read_csv('./website_classification.csv')
+    rand_num = random.randint(0, len(websites['website_url']))
+    website_name = re.findall(r"(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]", websites['website_url'][rand_num])[0]
+
+    if website_name[0] == 'w':
+        website_name = website_name[4:]
+    
+    #username generation
+    nicks = pd.read_csv('./nicknames.csv')
+    names = pd.read_csv('./names.csv', sep=',' ,error_bad_lines=False).dropna()
+    final_name = ''
+
+    use_nick = bool(random.getrandbits(1))
+
+    if use_nick:
+        rand_num = random.randint(0, len(nicks['nicknames']))
+        final_name = nicks['nicknames'][rand_num]
+    else: 
+        rand_first_num = random.randint(0, names.shape[0] - 1)
+        rand_second_num = random.randint(0, names.shape[0] - 1)
+        seps = ['_', '.']
+
+        final_name = names[names.columns[0]][rand_first_num] + seps[random.randint(0, len(sep))] + names[names.columns[0]][rand_second_num]
+
+    return final_name + "@" + website_name
+
+
+
+
+print(generate_email_address())
